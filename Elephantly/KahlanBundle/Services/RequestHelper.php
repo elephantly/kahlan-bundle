777fff5211e90e9bbd6a99252061254c1d70af20
Benjamin Zaslavsky <bzaslavsky@omnilog.fr>
2016-12-20 11:22:45 +0100

    Fixed RequestHelper
    Fixed hydration of request object for http methods other than GET

--- Elephantly/KahlanBundle/Services/RequestHelper.php
+++ Elephantly/KahlanBundle/Services/RequestHelper.php
@@ -9,6 +9,7 @@
 namespace Elephantly\KahlanBundle\Services;
 
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\RequestContext;
 use Symfony\Component\Routing\Router;
 
 class RequestHelper
@@ -37,24 +38,41 @@ class RequestHelper
      */
     public function getRequest($method = null, $url = null, array $parameters = array())
     {
-        $parsedUrl  = null !== $url ? parse_url($url) : array();
-        $method     = null == $method ? 'GET' : strtoupper($method);
+        try {
+            $object = new Request();
+            $object->setMethod($method);
+            $context = new RequestContext();
+            $context->fromRequest($object);
+            $this->router->setContext($context);
 
-        $query      = array();
-        if ('GET' == $method && !isset($parsedUrl['query'])) {
-            $query = $parameters;
-        } else if (isset($parsedUrl['query']) && null !== $parsedUrl['query']) {
-            $query = null !== parse_str($parsedUrl['query']) ? : array();
+            $parsedUrl  = null !== $url ? parse_url($url) : array();
+            $method     = null == $method ? 'GET' : strtoupper($method);
+
+            $query      = array();
+            if ('GET' == $method && !isset($parsedUrl['query'])) {
+                $query = $parameters;
+            } else if (isset($parsedUrl['query']) && null !== $parsedUrl['query']) {
+                $query = null !== parse_str($parsedUrl['query']) ? : array();
+            }
+
+            $request    = ('POST' || 'PUT') == $method ? $parameters : array() ;
+
+            $attributes = isset($parsedUrl['path']) && null !== $parsedUrl['path'] ? $this->router->match($parsedUrl['path']) : array();
+
+            $cookies    = array();
+            $files      = array();
+            $server     = array();
+
+            $object->query->add($query);
+            $object->request->add($request);
+            $object->attributes->add($attributes);
+            $object->cookies->add($cookies);
+            $object->files->add($files);
+            $object->server->add($server);
+        } catch (\Exception $e) {
+            return null;
         }
 
-        $request    = 'POST' == $method ? $parameters : array() ;
-
-        $attributes = isset($parsedUrl['path']) && null !== $parsedUrl['path'] ? $this->router->match($parsedUrl['path']) : array();
-
-        $cookies    = array();
-        $files      = array();
-        $server     = array();
-
-        return new Request($query, $request, $attributes, $cookies, $files, $server);
+        return $object;
     }
 }
