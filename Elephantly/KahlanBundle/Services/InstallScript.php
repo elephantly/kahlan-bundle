156a751f7a26708c43d2e73a2439e0cb8c0c8ba0
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2017-03-22 21:48:04 +0100

    Install script

---
+++ Elephantly/KahlanBundle/Services/InstallScript.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: benjamin
+ * Date: 22/03/17
+ * Time: 21:08
+ */
+
+namespace Elephantly\KahlanBundle\Services;
+
+use Sensio\Bundle\GeneratorBundle\Manipulator\KernelManipulator;
+
+class InstallScript
+{
+    public static function initBundle()
+    {
+        $kernel = new \AppKernel('prod', false);
+        $kernel->loadClassCache();
+
+        $kernelManipulator = new KernelManipulator($kernel);
+
+        $ret = $kernelManipulator->addBundle('Elephantly\KahlanBundle\KahlanBundle');
+    }
+}
